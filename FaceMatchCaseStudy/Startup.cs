a9aea014829bba65bc0a8b27b78f485909b7caf2
Adrian Toman <adrian@adriantoman.com>
2019-11-17 11:09:17 +1100

    Rename to project and namespaces to FaceMatchCaseStudy

--- FaceMatchCaseStudy/Startup.cs
+++ FaceMatchCaseStudy/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace FaceServiceWorkshop
+namespace FaceMatchCaseStudy
 {
     public class Startup
     {

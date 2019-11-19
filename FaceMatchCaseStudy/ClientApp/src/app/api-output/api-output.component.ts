167018e5c697de509d11b4cd346c74e9664d20db
Adrian Toman <adrian@adriantoman.com>
2019-11-19 13:20:18 +1100

    Remove unrequired components

--- FaceMatchCaseStudy/ClientApp/src/app/api-output/api-output.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'api-output',
-  templateUrl: './api-output.component.html'
-})
-export class ApiOutputComponent {
-}
--- FaceMatchCaseStudy/ClientApp/src/app/app.module.ts
+++ FaceMatchCaseStudy/ClientApp/src/app/app.module.ts
@@ -7,15 +7,11 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { ApiOutputComponent } from './api-output/api-output.component';
 import { FaceIndexComponent } from './face-index/face-index.component';
 import { FaceMatchComponent } from './face-match/face-match.component';
 import { AzureDetectComponent } from './azure-detect/azure-detect.component';
 import { AzureTrainComponent } from './azure-train/azure-train.component';
 import { AzureVerifyComponent } from './azure-verify/azure-verify.component';
-
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 import { PrettyPrintPipe } from "./azure-detect/pretty-print.pipe";
 
@@ -26,7 +22,6 @@ import { AzureFaceRecognitionService } from "./services/azure-face-recognition.s
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    ApiOutputComponent,
     FaceIndexComponent,
     FaceMatchComponent,
     AzureDetectComponent,
@@ -34,9 +29,6 @@ import { AzureFaceRecognitionService } from "./services/azure-face-recognition.s
     AzureVerifyComponent,
 
     PrettyPrintPipe,
-
-    CounterComponent,
-    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -44,11 +36,8 @@ import { AzureFaceRecognitionService } from "./services/azure-face-recognition.s
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'api-output', component: ApiOutputComponent },
       { path: 'face-match', component: FaceMatchComponent },
-      { path: 'face-index', component: FaceIndexComponent },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'face-index', component: FaceIndexComponent }
     ])
   ],
   providers: [
